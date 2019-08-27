b2ad6455474f768eb0667221223bdda91863f8eb
Bartek Błęcki <bartek@blecki.pl>
2019-08-27 14:25:21 +0200

    Update App.js

--- src/containers/App.js
+++ src/containers/App.js
@@ -1,19 +1,32 @@
 import React from 'react';
-import uuid from 'uuid';
+import uuid from 'uuid-v4';
 import style from './App.css';
-import Title from '../components/Title';
+import Title from './../components/Title';
+import TodoList from './../components/TodoList';
+import AddOption from './../components/TodoForm';
 
 class App extends React.Component {
     constructor(props){
         super(props);
+        this.addTodo = this.addTodo.bind(this);
+        this.removeTodo = this.removeTodo.bind(this);
         this.state = {
-            data: []
+            data: [{
+                id: 1,
+                    text: 'clean room'
+            }, {
+                id: 2,
+                    text: 'wash the dishes'
+            }, {
+                id: 3,
+                    text: 'feed my cat'
+            }]
         };
     }
     addTodo(val){
         const todo = {
             text: val,
-            id: uuid.v4(),
+            id: uuid(),
         };
         const data = [...this.state.data, todo];
         this.setState({data});
@@ -26,10 +39,11 @@ class App extends React.Component {
         return (
             <div className={style.TodoApp}>
                 <Title title={"Tytuł"} qty={this.state.data.length} />
+                <AddOption handleAddOption={this.addTodo} />
+                <TodoList list={this.state.data} remove={this.removeTodo} />
             </div>
-        );
+        )
     }
-}
+};
 
 export default App;
-
